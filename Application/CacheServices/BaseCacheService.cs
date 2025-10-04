fd799791845b26c367183c2e3e97de8a8eea7e94
Kristian Wessel Finstad <kristian.w.finstad@gmail.com>
2025-10-04 14:15:51 +0200

    Cacheservice logging removed

--- Application/CacheServices/BaseCacheService.cs
+++ Application/CacheServices/BaseCacheService.cs
@@ -37,13 +37,6 @@ public class BaseCacheService<TModel> : IBaseCacheService<TModel>
     {
         _cache.Clear();
         AddRange(models);
-        if (typeof(TModel) == typeof(DriverLicense))
-        {
-            foreach (var key in _cache.Keys)
-            {
-                AppLogger.LogDev($"key: {key}");
-            }
-        }
     }
 
     /// <summary>
